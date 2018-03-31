d2353029826ecc32df50eae94eeeda3b3eeb605f
Kamen Petrov <kamen.petrov.official@gmail.com>
2018-03-31 18:40:02 +0300

    Revert "Revert "trying to remove snake""
    This reverts commit 6a805ad87d3fd69b2792fe611afce074fc222ee8.

---
+++ Snake Game/Start/Game Classes/Point.cs
@@ -0,0 +1,58 @@
+﻿namespace Start.Game_Classes
+{
+    public class Point
+    {
+        private int x;
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                if (value >= 0 && value < Board.Width)
+                {
+                    x = value;
+                }
+            }
+        }
+
+        private int y;
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                if (value >= 0 && value < Board.Heigth)
+                {
+                    y = value;
+                }
+            }
+        }
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(Point other)
+        {
+            this.X = other.X;
+            this.Y = other.Y;
+        }
+
+        //Checks if two points are the same
+        public bool isEqualTo(Point other)
+        {
+            if (this.X == other.X && this.Y == other.Y)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
